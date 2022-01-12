f5d6066f89fad57075165d230a5ebf5b3510c8f7
kevinheavey <kevinheavey123@gmail.com>
2022-01-12 15:58:21 +0000

    change bench input size

--- bench.py
+++ bench.py
@@ -11,16 +11,16 @@ def test_inflection_underscore(benchmark):
 
 
 def test_snake_long_sentence(benchmark):
-    benchmark(snake, "DeviceType" * 10_000)
+    benchmark(snake, "DeviceType" * 100_000)
 
 
 def test_inflection_underscore_long_sentence(benchmark):
-    benchmark(underscore, "DeviceType" * 10_000)
+    benchmark(underscore, "DeviceType" * 100_000)
 
 
 def test_snake_many(benchmark):
-    benchmark(snake_many, ["DeviceType"] * 10_000)
+    benchmark(snake_many, ["DeviceType"] * 100_000)
 
 
 def test_inflection_underscore_many(benchmark):
-    benchmark(lambda lst: [underscore(x) for x in lst], ["DeviceType"] * 10_000)
+    benchmark(lambda lst: [underscore(x) for x in lst], ["DeviceType"] * 100_000)
