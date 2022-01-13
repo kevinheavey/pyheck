9cd6f90838dc149888e85e2c4d153ee48a6a363f
kevinheavey <kevinheavey123@gmail.com>
2022-01-13 14:24:52 +0000

    fix docs

--- bench.py
+++ bench.py
@@ -1,5 +1,24 @@
-from pyheck import snake, snake_many
+from multiprocessing import Pool, cpu_count
+from pyheck import snake
 from inflection import underscore
+
+
+def inflection_underscore_multiprocessing(strings: list[str]) -> list[str]:
+    try:
+        workers = cpu_count()
+    except NotImplementedError:
+        workers = 1
+    pool = Pool(processes=workers)
+    return pool.map(underscore, strings)
+
+
+def snake_multiprocessing(strings: list[str]) -> list[str]:
+    try:
+        workers = cpu_count()
+    except NotImplementedError:
+        workers = 1
+    pool = Pool(processes=workers)
+    return pool.map(snake, strings)
 
 
 def test_snake(benchmark):
@@ -18,9 +37,13 @@ def test_inflection_underscore_long_sentence(benchmark):
     benchmark(underscore, "DeviceType" * 100_000)
 
 
-def test_snake_many(benchmark):
-    benchmark(snake_many, ["DeviceType"] * 100_000)
-
-
 def test_inflection_underscore_many(benchmark):
     benchmark(lambda lst: [underscore(x) for x in lst], ["DeviceType"] * 100_000)
+
+
+def test_snake_multiprocessing(benchmark):
+    benchmark(snake_multiprocessing, ["DeviceType"] * 100_000)
+
+
+def test_inflection_underscore_multiprocessing(benchmark):
+    benchmark(inflection_underscore_multiprocessing, ["DeviceType"] * 100_000)
