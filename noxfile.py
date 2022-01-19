9a523fcc1835ebe82987c41c096d8db7d420b20d
kevinheavey <kevinheavey123@gmail.com>
2022-01-19 19:38:20 +0000

    add lint ci

--- noxfile.py
+++ noxfile.py
@@ -7,4 +7,3 @@ def python(session):
     session.install("pytest", "maturin", "sphinx")
     session.install(".", "--no-build-isolation")
     session.run("make", "test", external=True)
-    session.run("make", "lint", external=True)
